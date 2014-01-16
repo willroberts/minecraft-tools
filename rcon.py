21c88aa5c2bbd6f9bcbebfc0373dea025e7326fd
Will Roberts <will.roberts11@gmail.com>
2014-01-15 17:59:06 -0800

    Requires password, adds wiki links, abstracts authentication, adds quit options, cleans up shell output

--- rcon.py
+++ rcon.py
@@ -22,12 +22,13 @@ def parse_arguments(args=None):
     )
     parser.add_argument("--host", dest="host", default="127.0.0.1")
     parser.add_argument("--port", dest="port", default=25575)
-    parser.add_argument("--password", dest="password", default="")
+    parser.add_argument("--password", dest="password", required=True)
     return parser.parse_args()
 
 
 def create_packet(message, message_type):
     """ Creates a packet/struct based on the Minecraft RCON API specification.
+    See http://wiki.vg/Rcon#Packet_Format
     """
     header = struct.pack(
         "<iii",  # format: little-endian byte order, three integers
@@ -66,7 +67,28 @@ def process_response(client, response="", data_remains=True):
             1.0  # 1s timeout
         )[0]  # store the "read" field from the returned array
 
-    return response, response_type
+    return response, response_type, response_id
+
+
+def authenticate(client, password):
+    """ Sends an authentication packet to the RCON server (type
+    RCON_AUTHENTICATE), and checks the response for a matching message ID. A
+    message ID of -1 indicates authentication failure.
+    See http://wiki.vg/Rcon#3:_Login
+    """
+    auth_packet = create_packet(
+        password,
+        MessageTypes.RCON_AUTHENTICATE
+    )
+
+    client.send(auth_packet)
+
+    response, response_type, response_id = process_response(client)
+    if response_id == 0:
+        print "Authenticated."
+    else:
+        print "Failed to authenticate."
+        exit(1)
 
 
 def interact(client):
@@ -75,8 +97,13 @@ def interact(client):
     """
     while True:
 
-        # read a command and send it to the rcon server
+        # read a command and quit if requested
         command = raw_input("rcon> ")
+        if command in ["quit", "q"]:
+            print "Disconnecting."
+            return
+
+        # send the command to the rcon server
         packet = create_packet(
             command,
             MessageTypes.RCON_EXEC_COMMAND
@@ -84,7 +111,7 @@ def interact(client):
         client.send(packet)
 
         # read and process the response from the server
-        response, response_type = process_response(client)
+        response, response_type, response_id = process_response(client)
         if response:
             # add some newlines to help page output for cleanliness
             if command.startswith("help"):
@@ -100,29 +127,17 @@ def main(args=None):
     options = parse_arguments(args)
 
     # create a socket and connect to the minecraft server
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((options.host, int(options.port)))
-
-    # authenticate
-    auth_packet = create_packet(
-        options.password,
-        MessageTypes.RCON_AUTHENTICATE
-    )
-    client.send(auth_packet)
-    print "Connecting to %s:%d. Press Ctrl-C to exit." % (
+    print "Connecting to %s:%d..." % (
         options.host,
         int(options.port)
     )
-
-    # check for message type "RCON_AUTH_RESPONSE"
-    response, response_type = process_response(client)
-    if response_type == MessageTypes.RCON_AUTH_RESPONSE:
-        print "Authenticated."
-    else:
-        print "Failed to authenticate."
-        exit(1)
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((options.host, int(options.port)))
+    authenticate(client, options.password)
 
     # poll the user for commands to send
+    print "\n  Welcome to the rcon shell. Enter commands here to send them"
+    print "  to the RCON server. To quit, type \"quit\" or \"q\".\n"
     try:
         interact(client)
     except KeyboardInterrupt, k:
