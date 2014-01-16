6f5a5b37b049e3f0a0144adc8070b5b00d1371e2
Will Roberts <will.roberts11@gmail.com>
2014-01-16 15:18:53 -0800

    Updates comments, forces argument types, isolates terminal output to CLI components

--- rcon.py
+++ rcon.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import argparse
 import socket
 import select
@@ -7,6 +5,9 @@ import struct
 
 
 class MessageTypes(object):
+    """ Message types used by the RCON API. Only used when sending data at the
+    moment, but could be used to verify the types of incoming data.
+    """
     RCON_AUTHENTICATE = 3
     RCON_AUTH_RESPONSE = 2
     RCON_EXEC_COMMAND = 2
@@ -14,21 +15,26 @@ class MessageTypes(object):
 
 
 def parse_arguments(args=None):
-    """ Accepts arguments for host, port, and password. Sets defaults for
-    host and port. Note that a password is required for RCON.
+    """ Provides host, port, and password options for command-line use. The
+    RCON API requires a password.
     """
-    parser = argparse.ArgumentParser(
-        description="Connect to a Minecraft RCON server"
-    )
-    parser.add_argument("--host", dest="host", default="127.0.0.1")
-    parser.add_argument("--port", dest="port", default=25575)
-    parser.add_argument("--password", dest="password", required=True)
+    parser = argparse.ArgumentParser(description="Connect to a Minecraft RCON server")
+    parser.add_argument("--host", dest="host", type=str, default="127.0.0.1")
+    parser.add_argument("--port", dest="port", type=int, default=25575)
+    parser.add_argument("--password", dest="password", type=str, required=True)
     return parser.parse_args()
 
 
 def create_packet(message, message_type):
-    """ Creates a packet/struct based on the Minecraft RCON API specification.
-    See http://wiki.vg/Rcon#Packet_Format
+    """ Creates a packed header struct based on the message parameters,
+    and returns the combined header, message, and null byte padding.
+
+    Header Struct (little-endian):
+        Padded message length (int)
+        Message ID (int)
+        Message type (int)
+
+    Based on implementation details from http://wiki.vg/Rcon#Packet_Format
     """
     header = struct.pack(
         "<iii",  # format: little-endian byte order, three integers
@@ -39,61 +45,55 @@ def create_packet(message, message_type):
     return header + message + "\x00\x00"  # end with two null bytes
 
 
-def process_response(client, response="", data_remains=True):
-    """ Reads output from the client socket and formats it into a string.
+def process_response(client):
+    """ Reads the header from the client socket, then streams data until
+    no data is left to be read from the socket. The header struct has the
+    same format as in process_command(), so we read 12 bytes at a time (to
+    receive three 4-byte integers).
+
+    The read loop uses select() to check the client socket for remaining
+    data. The client socket is passed in for a readability check, and the
+    returned list indicates whether or not data remains to be read. When
+    the returned list is empty, the loop ends.
+
+    In this loop, we read from the socket until all data has been received,
+    then combine the response and return it along with the response ID,
+    which allows us to verify authentication attempts.
     """
     response_length, response_id, response_type = struct.unpack(
-        "<iii",  # same packet structure as before
-        client.recv(12)  # read three 4-byte integers
+        "<iii",
+        client.recv(12)
     )
-
-    # initialized as True, but eventually will be a list of the socket
-    # object ready for reads. when there's no data left to be read,
-    # data_remains will be an empty list and the loop will end.
+    response = ""
+    data_remains = True
     while data_remains:
-
-        # continue reading, but don't re-read the message id or type
         response_data = client.recv(response_length - 8)
-
-        # add the fragment to our response output
-        # strip the null bytes off each fragment
         response += response_data.strip("\x00\x00")
-
-        # check the client socket for reading
-        data_remains = select.select(
-            [client],  # check the socket for readability
-            [],  # no writability checks
-            [],  # no "exceptional condition" checks
-            1.0  # 1s timeout
-        )[0]  # store the "read" field from the returned array
-
-    return response, response_type, response_id
+        data_remains = select.select([client], [], [], 1.0)[0]
+    return response, response_id
 
 
 def authenticate(client, password):
-    """ Sends an authentication packet to the RCON server (type
-    RCON_AUTHENTICATE), and checks the response for a matching message ID. A
-    message ID of -1 indicates authentication failure.
-    See http://wiki.vg/Rcon#3:_Login
+    """ Sends an authentication packet to the RCON server and checks the
+    response for a matching message ID. A message ID of -1 indicates
+    authentication failure, and a matching message ID (0 in our case)
+    indicates success.
+
+    Based on implementation details from http://wiki.vg/Rcon#3:_Login
     """
     auth_packet = create_packet(
         password,
         MessageTypes.RCON_AUTHENTICATE
     )
-
     client.send(auth_packet)
-
-    response, response_type, response_id = process_response(client)
-    if response_id == 0:
-        print "Authenticated."
-    else:
-        print "Failed to authenticate."
-        exit(1)
+    response, response_id = process_response(client)
+    return response_id
 
 
-def interact(client):
-    """ Handles the back-and-forth communication between the client and the
-    remote server.
+def cli(client):
+    """ Reads a command from the prompt, sends it to the server, processes the
+    response, and returns the output to the user. Allows quitting with "quit"
+    or "q", and adds newlines to help page output to improve readability.
     """
     while True:
 
@@ -111,7 +111,7 @@ def interact(client):
         client.send(packet)
 
         # read and process the response from the server
-        response, response_type, response_id = process_response(client)
+        response, response_id = process_response(client)
         if response:
             # add some newlines to help page output for cleanliness
             if command.startswith("help"):
@@ -120,26 +120,19 @@ def interact(client):
 
 
 def main(args=None):
-    """ Interact with a Minecraft RCON server for remote administration.
-    """
-
-    # parse command-line arguments
     options = parse_arguments(args)
-
-    # create a socket and connect to the minecraft server
-    print "Connecting to %s:%d..." % (
-        options.host,
-        int(options.port)
-    )
+    print "Connecting to %s:%d..." % (options.host, options.port)
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((options.host, int(options.port)))
-    authenticate(client, options.password)
-
-    # poll the user for commands to send
+    client.connect((options.host, options.port))
+    if authenticate(client, options.password) == 0:
+        print "Authenticated."
+    else:
+        print "Failed to authenticate."
+        exit(1)
     print "\n  Welcome to the rcon shell. Enter commands here to send them"
     print "  to the RCON server. To quit, type \"quit\" or \"q\".\n"
     try:
-        interact(client)
+        cli(client)
     except KeyboardInterrupt, k:
         print "\nDisconnected."
         exit(0)
