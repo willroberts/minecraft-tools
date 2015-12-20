5bc3686f0d5b9df53be8c1e6f54bb7a0f6da2a9d
Will Roberts <will.roberts11@gmail.com>
2015-12-20 17:55:52 +0100

    Python 3 compatibility, general cleanup

--- rcon_shell.py
+++ rcon_shell.py
@@ -1,79 +1,82 @@
-from api.rcon import *
-from argparse import ArgumentParser
-from getpass import getpass
+import argparse
+import getpass
+import sys
 
-"""
-Minecraft RCON Client Console
-"""
+from api.rcon import *
+
+'''Minecraft RCON Client Console'''
 
 
 def parse_arguments():
-    """ Provides host and port options for command-line use.
+    '''Provides host and port options for command-line use.
 
     @return: options namespace based on provided arguments
-    """
-    parser = ArgumentParser(description="Connect to a Minecraft RCON server")
-    parser.add_argument(
-        "--host",
-        default="127.0.0.1",
-        help="default: 127.0.0.1"
+    '''
+    parser = argparse.ArgumentParser(
+        description='Connect to a Minecraft RCON server',
     )
     parser.add_argument(
-        "--port",
+        '--host',
+        default='127.0.0.1',
+        help='default: 127.0.0.1',
+    )
+    parser.add_argument(
+        '--port',
         type=int,
         default=25575,
-        help="default: 25575"
+        help='default: 25575',
     )
     return parser.parse_args()
 
 
 def rcon_shell(rcon):
-    """ Reads a command from the prompt, sends it to the server, processes the
-    response, and returns the output to the user. Allows quitting with "quit"
-    or "q", and adds newlines to help page output to improve readability.
+    '''Reads a command from the prompt, sends it to the server, processes the
+    response, and returns the output to the user. Allows quitting with 'quit'
+    or 'q', and adds newlines to help page output to improve readability.
 
     @param rcon: Instance of api.rcon.RemoteConsole()
-    """
-    print "\n  Welcome to the rcon shell. Enter commands here to send them"
-    print "  to the RCON server. To quit, type \"quit\" or \"q\".\n"
+    '''
+    print('\n  Welcome to the rcon shell. Enter commands here to send them')
+    print('  to the RCON server. To quit, type \'quit\' or \'q\'.\n')
     while True:
-        command = raw_input("rcon> ")
-        if command in ["quit", "q"]:
+        command = input('rcon> ')
+        if command in ('quit', 'q'):
             return
-        response, response_id = rcon.send(command)
+        response, _ = rcon.send(command)
         if response:
-            if command.startswith("help"):
-                response = response.replace("/", "\n/")
-            print response
+            if command.startswith('help'):
+                response = response.replace('/', '\n/')
+            print(response)
 
 
-def main():
-    """ Reads command-line arguments, connects to the RCON server, and starts
+def main(options, rcon=None):
+    '''Reads command-line arguments, connects to the RCON server, and starts
     the rcon_shell loop. Handles exceptions and interrupts, and disconnects
     when finished.
-    """
-    options = parse_arguments()
-    rcon = None
-    print "Connecting to %s:%d..." % (options.host, options.port)
-    password = getpass()
+    '''
+    print('Connecting to {}:{}...'.format(options.host, options.port))
+    password = getpass.getpass()
     try:
         rcon = RemoteConsole(
             options.host,
             options.port,
-            password
+            password,
         )
         rcon_shell(rcon)
     except ConnectionError:
-        print "Connection failed."
-        exit(1)
+        print('Connection failed.')
     except AuthenticationError:
-        print "Authentication failed."
+        print('Authentication failed.')
     except KeyboardInterrupt:
-        print
+        print()
     finally:
         if rcon:
             rcon.disconnect()
-        print "Disconnected."
+        print('Disconnected.')
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    # Use raw_input() as input() for Python 2
+    if sys.version_info.major == 2:
+        input = raw_input
+    options = parse_arguments()
+    main(options)
